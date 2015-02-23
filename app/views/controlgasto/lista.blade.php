abd058f9e73a134c8e1dcbd2559c1e18f0375364
fasme <mistiko2h@hotmail.com>
2015-02-23 18:25:59 -0300

    fecha order

--- app/views/controlgasto/lista.blade.php
+++ app/views/controlgasto/lista.blade.php
@@ -153,6 +153,11 @@ $("#example tfoot th").eq(7).html('<input type="text" size="1" placeholder="Busc
 
 var table  = $('#example').DataTable( {
 "iDisplayLength": -1,
+
+ "columnDefs": [ {
+      "targets": 0,
+      "type": 'date-eu'
+    } ],
         dom: 'T<"clear">lfrtip',
         tableTools: {
             "sSwfPath": "js/TableTools/swf/copy_csv_xls_pdf.swf",
